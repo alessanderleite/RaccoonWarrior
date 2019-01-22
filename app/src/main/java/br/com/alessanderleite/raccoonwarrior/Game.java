37b30fdf2176c26ff6c24e87b1b1a63e26347fbd
alessanderleite <alessander.logistica@gmail.com>
2019-01-22 08:11:37 -0200

    turn title off and set the full screen to the Game class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Game.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Game.java
@@ -2,12 +2,22 @@ package br.com.alessanderleite.raccoonwarrior;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //int this lesson we will set the content view and we will create a new class
         setContentView(R.layout.activity_game);
     }
 }
