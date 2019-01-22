7044364f9f2a8479d4213942cbdb260cffb13321
alessanderleite <alessander.logistica@gmail.com>
2019-01-22 18:04:04 -0200

    create the class named GamePanel

---
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/GamePanel.java
@@ -0,0 +1,32 @@
+package br.com.alessanderleite.raccoonwarrior;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    public GamePanel(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
