ffba8a74fbe94a729e7901d776fb39e056582d3e
alessanderleite <alessander.logistica@gmail.com>
2019-01-22 22:56:30 -0200

    code the methods created inside the GamePanel class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/GamePanel.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/GamePanel.java
@@ -17,22 +17,35 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
+    }
+
+    public void update() {
+        
     }
 }
