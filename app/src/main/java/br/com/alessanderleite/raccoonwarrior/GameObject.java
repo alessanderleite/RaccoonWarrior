8940c05a9437de28f94cb263cb5e13d342545cfe
alessanderleite <alessander.logistica@gmail.com>
2019-01-24 22:05:57 -0200

    add the variables, getters, setters and the Rect method inside the GameObject abstract class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/GameObject.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/GameObject.java
@@ -1,4 +1,41 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Rect;
+
 public abstract class GameObject {
+
+    protected int x;
+    protected int y;
+    protected int dx;
+    protected int dy;
+    protected int width;
+    protected int height;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Rect getRectangle() {
+        return new Rect(x, y, x + width, y + height);
+    }
 }
