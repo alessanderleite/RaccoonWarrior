ae12ea91e9f2ea11c267339c805a465e718098f3
alessanderleite <alessander.logistica@gmail.com>
2019-01-24 22:27:06 -0200

    add code inside the GamePanel class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/GamePanel.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/GamePanel.java
@@ -1,13 +1,20 @@
 package br.com.alessanderleite.raccoonwarrior;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
+    public static final int WIDTH = 856;
+    public static final int HEIGHT = 480;
+
     private MainThread thread;
+
+    private Background bg;
 
     public GamePanel(Context context) {
         super(context);
@@ -19,11 +26,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         setFocusable(true);
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        thread.setRunning(true);
-        thread.start();
-    }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -31,7 +33,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
         while (retry) {
             try {
@@ -44,11 +46,38 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.mybgimage));
+
+        bg.setVector(-5);
+
+        thread.setRunning(true);
+        thread.start();
+    }
+
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
     }
 
     public void update() {
 
+        bg.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        final float scaleFactorX = getWidth()/(WIDTH*1.f);
+        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
+
+        if (canvas != null) {
+            final  int savedState = canvas.save();
+            canvas.scale(scaleFactorX, scaleFactorY);
+            bg.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
     }
 }
