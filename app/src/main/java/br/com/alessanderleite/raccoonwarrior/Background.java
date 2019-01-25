9172a6b2d8ea1cb299f3f69494e35a909434fbc2
alessanderleite <alessander.logistica@gmail.com>
2019-01-24 22:27:46 -0200

    add code inside the Background class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Background.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Background.java
@@ -1,12 +1,34 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Background {
 
-    public Background() {}
+    private Bitmap image;
+    private int x, y, dx;
 
-    public void update() {}
+    public Background(Bitmap res) {
+        image = res;
+    }
 
-    public void draw(Canvas canvas) {}
+    public void update() {
+        x += dx;
+
+        if (x < - GamePanel.WIDTH) {
+            x = 0;
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+
+        if (x < 0) {
+            canvas.drawBitmap(image, x + GamePanel.WIDTH, y, null);
+        }
+    }
+
+    public void setVector(int dx) {
+        this.dx = dx;
+    }
 }
