63005d3e2a53ffe46f16a1dd5425a10ef5311d21
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 23:59:00 -0200

    add code in the Hero class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
@@ -3,7 +3,7 @@ package br.com.alessanderleite.raccoonwarrior;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Hero {
+public class Hero extends GameObject {
 
     private Bitmap spritesheet;
     private int score;
@@ -11,7 +11,29 @@ public class Hero {
     private boolean up;
     private boolean playing;
 
-    public Hero() {}
+    private Animation animation = new Animation();
+    private long startTime;
+
+    public Hero(Bitmap res, int w, int h, int numFrames) {
+        x = 100;
+        y = GamePanel.HEIGHT / 2;
+        dy = 0;
+        score = 0;
+        height = h;
+        width = w;
+
+        Bitmap[] image = new Bitmap[numFrames];
+        spritesheet = res;
+
+        for (int i = 0; i < image.length; i++) {
+            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(10);
+
+        startTime = System.nanoTime();
+    }
 
     public void update() {}
 
