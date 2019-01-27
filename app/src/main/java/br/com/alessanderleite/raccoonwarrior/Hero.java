b920d79919e353b1ec4d27094007d81045482cb1
alessanderleite <alessander.logistica@gmail.com>
2019-01-27 11:14:52 -0200

    code the methods inside the Hero class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
@@ -35,7 +35,54 @@ public class Hero extends GameObject {
         startTime = System.nanoTime();
     }
 
-    public void update() {}
+    public void setUp(boolean b) {
+        up = b;
+    }
 
-    public void draw(Canvas canvas) {}
+    public void update() {
+        long elapsed = (System.nanoTime() - startTime) / 1000000;
+
+        if (elapsed > 100) {
+            score++;
+            startTime = System.nanoTime();
+        }
+        animation.update();
+
+        if (up) {
+            dy  = (int) (dya -= 1.1);
+        }
+        else {
+            dy = (int)(dya += 1.1);
+        }
+
+        if (dy > 14) dy = 14;
+        if (dy <-14) dy = -14;
+
+        y += dy*2;
+        dy = 0;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(animation.getImage(), x, y, null);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean getPlaying() {
+        return playing;
+    }
+
+    public void setPlaying(boolean b) {
+        playing = b;
+    }
+
+    public void resetDYA() {
+        dya = 0;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
 }
