934db1d25da3504b38e998658aa35b064cd0966c
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 23:16:59 -0200

    delete the vector method

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Background.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Background.java
@@ -10,6 +10,7 @@ public class Background {
 
     public Background(Bitmap res) {
         image = res;
+        dx = GamePanel.MOVESPEED;
     }
 
     public void update() {
@@ -26,9 +27,5 @@ public class Background {
         if (x < 0) {
             canvas.drawBitmap(image, x + GamePanel.WIDTH, y, null);
         }
-    }
-
-    public void setVector(int dx) {
-        this.dx = dx;
     }
 }
