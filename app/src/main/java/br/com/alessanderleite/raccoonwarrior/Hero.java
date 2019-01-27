93e7284861e8939ff718ebcbe44901d9e714b61a
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 23:12:00 -0200

    add Bitmap object and variables into the Hero class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Hero.java
@@ -1,8 +1,15 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Hero {
+
+    private Bitmap spritesheet;
+    private int score;
+    private double dya;
+    private boolean up;
+    private boolean playing;
 
     public Hero() {}
 
