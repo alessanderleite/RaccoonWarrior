9893cf8712b399c68af44017400fca3d769b60e1
alessanderleite <alessander.logistica@gmail.com>
2019-01-28 12:20:52 -0200

    make the Splash class extends AppCompatActivity and call the onCreate method

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Splash.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Splash.java
@@ -1,4 +1,30 @@
 package br.com.alessanderleite.raccoonwarrior;
 
-public class Splash {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent startGame = new Intent(getApplicationContext(), Game.class);
+                    startActivity(startGame);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
 }
