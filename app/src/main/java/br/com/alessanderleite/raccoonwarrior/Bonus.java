4f79a522484c495376070399efeecb58ede07738
alessanderleite <alessander.logistica@gmail.com>
2019-01-28 19:03:08 -0200

    code the Bonus class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Bonus.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Bonus.java
@@ -1,12 +1,53 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Bonus {
+import java.util.Random;
 
-    public Bonus() {}
+public class Bonus extends GameObject {
 
-    public void update() {}
+    private int score;
+    private int speed;
+    private Random rand = new Random();
+    private Animation animation = new Animation();
+    private Bitmap spritesheet;
 
-    public void draw(Canvas canvas) {}
+    public Bonus(Bitmap res, int x, int y, int w, int h, int numFrames) {
+        super.x = x;
+        super.y = y;
+        width = w;
+        height = h;
+
+        speed = 4 + (int)(rand.nextDouble()*score/30);
+        if (speed > 40)speed = 40;
+
+        Bitmap[] image = new Bitmap[numFrames];
+        spritesheet = res;
+
+        for (int i = 0; i < image.length; i++) {
+            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0,width, height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(100-speed);
+    }
+
+    public void update() {
+        x -= speed;
+        animation.update();
+    }
+
+    public void draw(Canvas canvas) {
+        try {
+            canvas.drawBitmap(animation.getImage(), x, y, null);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return width - 10;
+    }
 }
