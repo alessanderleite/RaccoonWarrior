fe5791d1d6880368bc4798aab6f6c48be7a7f74f
alessanderleite <alessander.logistica@gmail.com>
2019-01-28 10:03:28 -0200

    code the Explosion class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Explosion.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Explosion.java
@@ -1,12 +1,47 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Explosion {
+public class Explosion extends GameObject {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int row;
+    private Animation animation = new Animation();
+    private Bitmap spritesheet;
 
-    public Explosion() {}
+    public Explosion(Bitmap res, int x, int y, int w, int h, int numFrames) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        Bitmap[] image = new Bitmap[numFrames];
+        spritesheet = res;
 
-    public void update() {}
+        for (int i = 0; i < image.length; i++) {
+            if (i%5 == 0 && i > 0)row++;
+            image[i] = Bitmap.createBitmap(spritesheet, (i - (5*row))*width, row*height, width, height);
+        }
 
-    public void draw(Canvas canvas) {}
+        animation.setFrames(image);
+        animation.setDelay(7);
+    }
+
+    public void update() {
+        if (!animation.playedOnce()) {
+            animation.update();
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        if (!animation.playedOnce()) {
+            canvas.drawBitmap(animation.getImage(), x, y, null);
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
