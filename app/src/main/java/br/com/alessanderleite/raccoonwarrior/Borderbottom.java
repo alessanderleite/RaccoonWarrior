b57e0e289e03c12a87d800bbba1b79803f836ae5
alessanderleite <alessander.logistica@gmail.com>
2019-01-27 23:31:08 -0200

    code the Borderbottom class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Borderbottom.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Borderbottom.java
@@ -1,12 +1,26 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Borderbottom {
+public class Borderbottom extends GameObject {
 
-    public Borderbottom() {}
+    private Bitmap image;
 
-    public void update() {}
+    public Borderbottom(Bitmap res, int x, int y) {
+        height = 150;
+        width = 20;
+        this.x = x;
+        this.y = y;
+        dx = GamePanel.MOVESPEED;
+        image = Bitmap.createBitmap(res, 0, 0 , width, height);
+    }
 
-    public void draw(Canvas canvas) {}
+    public void update() {
+        x += dx;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+    }
 }
