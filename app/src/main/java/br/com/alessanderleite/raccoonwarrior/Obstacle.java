f0d71c44839fe134ed7d19e9b011ee9109a0777b
alessanderleite <alessander.logistica@gmail.com>
2019-01-28 09:12:39 -0200

    add code inside Obstacle class

--- app/src/main/java/br/com/alessanderleite/raccoonwarrior/Obstacle.java
+++ app/src/main/java/br/com/alessanderleite/raccoonwarrior/Obstacle.java
@@ -1,12 +1,53 @@
 package br.com.alessanderleite.raccoonwarrior;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Obstacle {
+import java.util.Random;
 
-    public Obstacle() {}
+public class Obstacle extends GameObject {
 
-    public void update() {}
+    private int score;
+    private int speed;
+    private Random rand = new Random();
+    private Animation animation = new Animation();
+    private Bitmap spritesheet;
 
-    public void draw(Canvas canvas) {}
+    public Obstacle(Bitmap res, int x, int y , int w, int h, int s, int numFrames) {
+        super.x = x;
+        super.y = y;
+        width = w;
+        height = h;
+        score = s;
+        speed = 11;
+
+        Bitmap[] image  = new Bitmap[numFrames];
+        spritesheet = res;
+
+        for (int i = 0; i < image.length; i++) {
+            image[i] = Bitmap.createBitmap(spritesheet, 1*width, 0, width, height);
+        }
+
+        animation.setFrames(image);
+        animation.setDelay(100 - speed);
+    }
+
+    public void update() {
+
+        x -= speed;
+        animation.update();
+    }
+
+    public void draw(Canvas canvas) {
+        try {
+            canvas.drawBitmap(animation.getImage(), x, y, null);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return width - 10;
+    }
 }
